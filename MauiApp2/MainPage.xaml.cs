649a35562db3e3685a3655ea74468db87ad6f5b1
1672055 <1672055@TE-GDA-D-D21E21>
2024-09-16 10:29:17 +0200

    Dodaj pliki projektów.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        private static readonly Random _random = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRollButtonClicked(object sender, EventArgs e)
+        {
+            int maxValue = 6;
+
+            if (RbK4.IsChecked)
+                maxValue = 4;
+            else if (RbK10.IsChecked)
+                maxValue = 10;
+            else if (RbK12.IsChecked)
+                maxValue = 12;
+
+            int result = _random.Next(1, maxValue + 1);
+            ResultLabel.Text = $"Wynik: {result}";
+        }
+    }
+}
+namespace MauiApp2
+{
+}
